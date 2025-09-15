7de56168f13839e8ad22a6fa332f8e4278f37704
pawan1sanjana <44228622+pawan1sanjana@users.noreply.github.com>
2025-09-15 12:19:46 +0530

    Delete js directory

--- js/404.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("popup").style.display = "block";
-});
-
-function closePopup() {
-    window.history.back(); // Go back to the previous page
-}
